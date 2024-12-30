372622acd8b1d690f949fdbee21f85bb13897eb8
eklerkjke <eklerkjke@gmail.com>
2024-12-30 19:42:00 +0500

    спринт 14 / Вторая версия

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j // Аннотация для автоматической генерации логгера
 public class NotFoundException extends RuntimeException {
