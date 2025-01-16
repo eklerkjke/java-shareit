75426c0a41d23f27722cd82683fde7c715653014
eklerkjke <eklerkjke@gmail.com>
2025-01-16 22:41:25 +0500

    спринт 16 / первая версия

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ItemRequestDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
