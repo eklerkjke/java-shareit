05f3f32d66b9ef52c7539c9dffdeaf3fc4af15f5
eklerkjke <eklerkjke@gmail.com>
2025-01-18 12:13:00 +0500

    спринт 16 / первая версия

--- server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.booking.service;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,7 +29,6 @@ public class BookingServiceImpl implements BookingService {
     private final ItemRepository itemRepository;
     private final UserRepository userRepository;
     private final BookingValidation validation;
-//    @Getter
     private final BookingResolver resolver;
 
     @Override
