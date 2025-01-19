3b25118c0e76b74beb486cb40410305d8bebbfea
eklerkjke <eklerkjke@gmail.com>
2025-01-19 12:40:13 +0500

    Спринт 16 / Вторая версия

--- gateway/src/main/java/ru/practicum/shareit/util/Headers.java
+++ gateway/src/main/java/ru/practicum/shareit/util/Headers.java
@@ -1,5 +1,5 @@
 package ru.practicum.shareit.util;
 
 final public class Headers {
-    final public static String HEADER_USER_ID = "X-Sharer-User-Id";
+    public final static String HEADER_USER_ID = "X-Sharer-User-Id";
 }
